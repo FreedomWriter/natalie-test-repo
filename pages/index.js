8d1f5c52ba602cb89bac38537a464dd65c278364
Natalie Davis <50391354+FreedomWriter@users.noreply.github.com>
2022-02-10 16:53:07 -0600

    test-commit-two

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
       <main>
         <Header title="Welcome to my app!" />
         <p className="description">
-          Get started now by editing <code>pages/index.js</code>
+          GI have started <code>pages/index.js</code>
         </p>
       </main>
 
